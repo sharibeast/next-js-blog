dd3c24bac64252f581709efa35dd597f2850a538
unknown <sharibeast@gmail.com>
2021-06-15 23:26:02 +0300

    build: wrapping the whole app with a layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
 
+import Layout from "../../components/layout";
 export default function FirstPost() {
-    return (
-        <div>
-            <h1>this is first post</h1>
-        </div>
-    )
+  return (
+    <Layout>
+      <div>
+        <Head>
+          <title>First post</title>
+        </Head>
+        <h1>this is first post</h1>
+        <Link href="/">
+          <a>back home</a>
+        </Link>
+      </div>
+    </Layout>
+  );
 }
